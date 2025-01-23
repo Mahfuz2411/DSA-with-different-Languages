113757a77a2f3ee28d3ca9e2222582cff14fa3da
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-01-23 21:42:36 +0600

    :sparkles: graph implementation of adj. matrix with CPP

--- Data_Structures/Graph/AdjMatrix/C++/adj-matrix-graph.cc
+++ Data_Structures/Graph/AdjMatrix/C++/adj-matrix-graph.cc
@@ -1,1 +1,40 @@
-// code will be added soon
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;  
+    cout << "Enter the number nodes: ";
+    cin >> n;
+
+    int graph[n][n];
+
+    for(int i=0; i<n; i++) {
+        int j;
+        for(j=0; j<n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    int m;
+    cout << "Enter the number of edges: ";
+    cin >> m;
+
+    for(int i=0; i<m; i++) {
+        int a, b; 
+
+        cin>> a>> b;
+
+        graph[a][b] = 1;
+        graph[b][a] = 1;
+    }
+
+    
+    for(int i=0; i<n; i++) {
+        int j;
+        for(j=0; j<n; j++) {
+            cout << graph[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
